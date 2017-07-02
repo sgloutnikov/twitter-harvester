01fcfdf580d47a8f65f6455abf9d97f02f6245cc
Stefan Gloutnikov <stefan@gloutnikov.com>
2017-07-01 18:02:47 -0700

    Setup heroku deployment

--- harvester.py
+++ harvester.py
@@ -1,12 +1,19 @@
 import twitter
 import os
-import json
+from pymongo import MongoClient, DESCENDING, ASCENDING
 
+
+print("Starting harvester...")
 # Twitter API Credentials
 consumer_key = 'DCe7buvPLhAXoBEEptW5w'
 access_token = '1025063461-f3m1fLtohYx5a7HILOgFu5R23eY1yvPJCT9cwTm'
 consumer_secret = os.environ.get('CONSUMER_SECRET')
 access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+
+MONGODB_URI = os.environ.get('MONGODB_URI')
+
+client = MongoClient(MONGODB_URI)
+db = client.get_default_database()
 
 
 api = twitter.Api(consumer_key=consumer_key,
@@ -15,11 +22,14 @@ api = twitter.Api(consumer_key=consumer_key,
                   access_token_secret=access_token_secret)
 
 tracked_hashtags = [
-    "#nba"
+    "#agt"
 ]
+
+print("Beginning to listen to Streaming API")
 
 stream = api.GetStreamFilter(track=tracked_hashtags)
 
 for line in stream:
-    print(json.dumps(line))
+    result = db.twitter_dump.insert_one(line)
+    print(result)
 
